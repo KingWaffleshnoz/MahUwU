f97fea73595a4495523fb01758a5b8f1a3f0c1e1
Jared Lyon <jaredalyon@gmail.com>
2020-03-20 21:00:34 -0700

    fix embeds

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,7 +3,7 @@ var Discord = require('discord.js');
 exports.run = (bot, member) => {
     if (member.guild.id == '461738525768024075') {
         var channel = member.guild.channels.cache.get("465742371670327296");
-        var join = new Discord.RichEmbed()
+        var join = new Discord.MessageEmbed()
             .setAuthor(member.user.username, member.user.avatarURL())
             .setFooter(member.guild.name)
             .setTimestamp()
@@ -15,7 +15,7 @@ exports.run = (bot, member) => {
         })
     } else if (member.guild.id == '479856759184424990') {
         var channel = member.guild.channels.cache.get("479916795671609345");
-        var join = new Discord.RichEmbed()
+        var join = new Discord.MessageEmbed()
             .setAuthor(member.user.username, member.user.avatarURL())
             .setFooter(member.guild.name)
             .setTimestamp()
