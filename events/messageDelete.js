051e4bec0f3bd0ee8fef77753c589722aecd8ca5
Jared Lyon <jaredalyon@gmail.com>
2020-03-20 20:23:37 -0700

    update events for discordjs v12

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -5,10 +5,10 @@ exports.run = (bot, messageDelete) => {
         if (messageDelete.channel.id == '466882599596326922') {
             return;
         } else if (messageDelete.channel.id != '466882599596326922') {
-            messageDelete.guild.channels.get("465742371670327296").send(`The message: "${messageDelete.content || "(no content)"}" by **${messageDelete.author.tag}** was deleted.`)
+            messageDelete.guild.channels.cache.get("465742371670327296").send(`The message: "${messageDelete.content || "(no content)"}" by **${messageDelete.author.tag}** was deleted.`)
         }
     } else if (messageDelete.guild.id == '479856759184424990' && !messageDelete.author.bot) {
-        messageDelete.guild.channels.get("479916795671609345").send(`The message: "${messageDelete.content || "(no content)"}" by **${messageDelete.author.tag}** was deleted.`)
+        messageDelete.guild.channels.cache.get("479916795671609345").send(`The message: "${messageDelete.content || "(no content)"}" by **${messageDelete.author.tag}** was deleted.`)
     } else {
         return;
     }
